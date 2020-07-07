669ca064676973319017378fa35548e4bda4c94a
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-07-06 23:07:05 -0400

    feat(ScraperParkingUCN/CivilData): add rut scraper project

---
+++ ScraperParkingUCN/src/main/java/cl/ucn/disc/pdis/simplescraper/CivicData.java
@@ -0,0 +1,113 @@
+package cl.ucn.disc.pdis.simplescraper;
+
+import cl.ucn.disc.pdis.simplescraper.dbformater.DbInteraction;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Random;
+
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+
+/**
+ * App to do scraping at nombrerutyfirma.cl
+ *
+ * @author Charlie Condorcet
+ */
+public class CivicData {
+
+    /**
+     * The Logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(CivicData.class);
+
+    /**
+     * Principal Main Class
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws IOException, SQLException {
+
+        DbInteraction DB = new DbInteraction();
+        Random random = new Random();
+
+        try {
+
+            for (int i = 0; i < DB.GetLengthFunctionary(); i++) {
+
+                String actualUrl = "https://www.nombrerutyfirma.com/buscar?term=" + DB.GetFunctionaryById(i+1);
+                Document document = null;
+
+                try {
+
+                    document = Jsoup.connect(actualUrl).get();
+                } catch (SocketTimeoutException e) {
+
+                    log.error("Timeout for http request. Details: {}", e.getMessage());
+                }
+
+                ArrayList<String> variablesCompletas = new ArrayList<>();
+
+                //  get all data with 'td' tag
+                Elements enteraPo =document.getElementsByTag("td");
+
+                // parting of 'td' tags
+                for (int j = 0; j < 5; j++) {
+
+                    variablesCompletas.add(enteraPo.first().text().toString());
+                    enteraPo.remove(enteraPo.first());
+                }
+
+                // Here we already got all the data from nombrerutyfirma separated by position.
+                /*
+                for (int j = 0; j < variablesCompletas.size(); j++) {
+
+                    // All data of one person.
+                    System.out.println(variablesCompletas.get(j));
+                }*/
+
+                // Data extracted from telephone directory.
+                String nombre = variablesCompletas.get(0);
+                String rut = variablesCompletas.get(1);
+                String sexo = variablesCompletas.get(2);
+                String direccion = variablesCompletas.get(3);
+                String comuna = variablesCompletas.get(4);
+
+                // Add new valid functionary to database.
+                boolean notExistInDb = DB.formatPublicInfo(nombre, rut, sexo, direccion, comuna);
+
+                if (notExistInDb) {
+
+
+                    // Time to wait not to do DDoS.
+                    try {
+                        Thread.sleep(1000 + random.nextInt(1000));
+
+                    } catch (InterruptedException e) {
+                        log.error("Thread is interrupted either before or during the activity. Details: {}", e.getMessage());
+
+                    }
+
+                } else {
+                    log.info("The new functionary is not added.");
+
+                }
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        //connectionSource.close();
+        log.info("End of insertions.");
+        DB.CloseDBConnection();
+    }
+}
