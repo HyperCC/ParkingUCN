0389c21e950c1eb87123f3aa7c8c363f8100ad1a
CharlieCondorcet <charliecondorcet@gmail.com>
2020-07-10 23:37:30 -0400

    feat(Server/Service class): add interface, implement IHostedService

--- Server/ServerParkingUcnService.cs
+++ Server/ServerParkingUcnService.cs
@@ -1,9 +1,106 @@
+using System;
+using Ice;
+using ServerZeroIce.model;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging; 
 
 namespace ServerParkingUCN
 {
-    internal class ServerParkingUcnService : IHostedService
-    {
     
+    internal class ServerParkingUcnService : IHostedService, IDisposable
+    {
+
+        /// <summary>
+        /// The Logger.
+        /// </summary>
+        private readonly ILogger<ServerParkingUcnService> _logger;
+
+        /// <summary>
+        /// The Port.
+        /// </summary>
+        private readonly int _port = 8080;
+
+        /// <summary>
+        /// The Communicator.
+        /// </summary>
+        private readonly Communicator _communicator;
+
+        /// <summary>
+        /// The ServerParkingUcnService.
+        /// </summary>
+        /// <param name="logger">Used to print debug message.</param>
+        public ServerParkingUcnService(ILogger<ServerParkingUcnService> logger)
+        {
+            _logger = logger; 
+            _communicator = buildCommunicator();
+
+        }
+
+        /// <summary>
+        /// Triggered when the application host is ready to start the service.
+        /// </summary>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Starting the FivetService ..");
+
+            // The adapter: https://doc.zeroc.com/ice/3.7/client-side-features/proxies/proxy-and-endpoint-syntax
+            // tcp (protocol) -z (compression) -t 15000 (timeout in ms) -p 8080 (port to bind)
+            var adapter = _communicator.createObjectAdapterWithEndpoints("TheSystem", "tcp -z -t 15000 -p " + _port);
+
+            // The interface
+            Contratos contratos = new ContratosImpl();
+
+            // Register in the communicator
+            adapter.add(contratos, Util.stringToIdentity("TheSystem"));
+
+            // Activation
+            adapter.activate();
+
+            // All ok
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Triggered when the application host is performing a graceful shutdown.
+        /// </summary>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Stopping the FivetService ..");
+
+            _communicator.shutdown();
+
+            _logger.LogDebug("Communicator Stopper!");
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Build the communicator.
+        /// </summary>
+        /// <returns>The Communicator</returns>
+        private Communicator buildCommunicator()
+        {
+            _logger.LogDebug("Initializating Communicator v{0} ({1}) ..", Ice.Util.stringVersion(), Ice.Util.intVersion());
+
+            // ZeroC properties
+            Properties properties = Util.createProperties();
+            // properties.setProperty("Ice.Trace.Network", "3");
+
+            InitializationData initializationData = new InitializationData();
+            initializationData.properties = properties;
+
+            return Ice.Util.initialize(initializationData);
+        }
+
+    }
+
+    /// <summary>
+    /// The Implementation of TheSystem interface.
+    /// </summary>
+    public class ContratosImpl : ContratosDisp_
+    {
+
     }
 
 }
