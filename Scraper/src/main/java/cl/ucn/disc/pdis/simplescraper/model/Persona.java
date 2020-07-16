508075d7ce306e589bb7ecfef77acbdfe3789fe9
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-07-16 19:32:07 -0400

    docs(Scraper/Persona): fix documentation

--- Scraper/src/main/java/cl/ucn/disc/pdis/simplescraper/model/Persona.java
+++ Scraper/src/main/java/cl/ucn/disc/pdis/simplescraper/model/Persona.java
@@ -28,7 +28,7 @@ import com.j256.ormlite.table.DatabaseTable;
 public final class Persona {
 
     /**
-     * The id: Primary Key.     *
+     * The id: Primary Key.
      */
     @DatabaseField(generatedId = true)
     private int id;
@@ -259,5 +259,4 @@ public final class Persona {
     public String getComuna() {
         return comuna;
     }
-
 }
