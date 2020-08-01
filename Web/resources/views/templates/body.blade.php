a5c56ab7b046baa0912573232bb4b175cb552234
CharlieCondorcet <charliecondorcet@gmail.com>
2020-07-31 20:47:31 -0400

    feat(Web/views): add template to html body

---
+++ Web/resources/views/templates/body.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8"/>
+    <title>Bitacora DISC - @yield('title_head')</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <link rel="shortcut icon" href="{{{ asset('img/favicon.png') }}}">
+
+</head>
+<body>
+
+<div class="d-flex flex-column h-screen justify-content-between" id="app">
+
+</div>
+
+</body>
+</html>
