767e4f7403a3b74afe8970cb9ffbc6154587a2f9
CharlieCondorcet <charliecondorcet@gmail.com>
2020-08-01 02:59:15 -0400

    feat(Web/home.blade): add banner and welcome to user

--- Web/resources/views/home.blade.php
+++ Web/resources/views/home.blade.php
@@ -8,6 +8,33 @@
 
         <div class="row">
 
+            <div class="col-12 col-lg-6">
+                <img class="img-fluid m-lg-3 m-sm-3" src="img/parkinghome.png" alt="parking ucn">
+            </div>
+
+            <div class="col-12 col-lg-6">
+
+                <div class="card-deck my-3 text-center">
+
+                    <!-- TITULO Y BIENVENIDA AL USUARIO -->
+
+                    <div class="card my-4 box-shadow">
+                        <div class="card-header bg-dark py-4">
+                            <h3 class="my-0 font-weight-normal text-light"> Bienvenido Señor Guardia </h3>
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title pircing-card-title py-4"> ¡Desde este panel de administración podrás
+                                gestionar todas tus operaciones para el parking en la UCN!
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="row">
 
             <div class="card-deck my-3 text-center">
 
@@ -45,7 +72,8 @@
                             <h4 class="my-0 font-weight-normal text-light"> Guardar nuevo Usuario </h4>
                         </div>
                         <div class="card-body">
-                            <p class="text-muted"> Crear un usuario y registrarlo junto a los demas usuarios autorizados</p>
+                            <p class="text-muted"> Crear un usuario y registrarlo junto a los demas usuarios
+                                autorizados</p>
                         </div>
                         <a href="#" class="btn mx-3 mb-3 btn-outline-success rounded-pill">
                             ¡A Inscribir!
@@ -59,7 +87,8 @@
                             <h4 class="my-0 font-weight-normal text-light"> Guardar nuevo Vehiculo </h4>
                         </div>
                         <div class="card-body">
-                            <p class="text-muted"> Crear un vehiculo validado el usuario como autorizado o registrado</p>
+                            <p class="text-muted"> Crear un vehiculo validado el usuario como autorizado o
+                                registrado</p>
                         </div>
                         <a href="#" class="btn mx-3 mb-3 btn-outline-success rounded-pill">
                             ¡A Inscribir!
