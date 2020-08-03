c24dc65d295b76279b30662bca4174ac2ee6c539
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-08-03 16:53:53 -0400

    feat(App): add slice files to project

--- App/app/src/main/java/cl/ucn/disc/pdis/app/MainActivity.kt
+++ App/app/src/main/java/cl/ucn/disc/pdis/app/MainActivity.kt
@@ -3,6 +3,7 @@ package cl.ucn.disc.pdis.app
 import android.content.Intent
 import cl.ucn.disc.pdis.app.Buscar
 import cl.ucn.disc.pdis.app.RegistroVehiculo
+import com.zeroc.Ice.*
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -16,14 +17,22 @@ import cl.ucn.disc.pdis.app.ui.AppTheme
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_buscar)
+        setContentView(R.layout.activity_main)
 
         val busquedaMain = findViewById<Button>(R.id.Buscar)
         val registrarMain = findViewById<Button>(R.id.Registrar)
-
-
-
+        setUiTransition(busquedaMain, registrarMain)
     }
 
+    private fun setUiTransition(busquedaMain: Button, registrarMain: Button){
+
+        busquedaMain.setOnClickListener {
+            startActivity(Intent(this, Buscar::class.java))
+        }
+
+        registrarMain.setOnClickListener {
+            startActivity(Intent(this, RegistroVehiculo::class.java))
+        }
+    }
 }
 
