a405af67c13dae5369c81711041468e5ad0c36fb
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-05 16:04:52 -0400

    feat(APP): add RegistroVehiculo class

---
+++ App/app/src/main/java/cl/ucn/dics/pdis/appparkingucn/RegistroVehiculo.java
@@ -0,0 +1,14 @@
+package cl.ucn.dics.pdis.appparkingucn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroVehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+
+    }
+}
