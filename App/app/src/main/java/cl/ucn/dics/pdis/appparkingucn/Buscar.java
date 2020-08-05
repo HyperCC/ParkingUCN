99995938c3714ab5aa3d1c48d6521b140104008c
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-08-05 15:59:37 -0400

    feat(App): add some functionalities to Buscar

--- App/app/src/main/java/cl/ucn/dics/pdis/appparkingucn/Buscar.java
+++ App/app/src/main/java/cl/ucn/dics/pdis/appparkingucn/Buscar.java
@@ -2,6 +2,8 @@ package cl.ucn.dics.pdis.appparkingucn;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class Buscar extends AppCompatActivity {
 
@@ -9,6 +11,32 @@ public class Buscar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_buscar);
+        Button botonBuscar = findViewById(R.id.botonBuscar);
+        RadioGroup seleccion = findViewById(R.id.seleccion);
+
+       botonBuscar.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               if (seleccion.getCheckedRadioButtonId() == R.id.botonRut) {
+                   //TODO: buscar rut
+                   Toast.makeText(Buscar.this, "El valor seleccionado es: "+ seleccion.getCheckedRadioButtonId(), Toast.LENGTH_SHORT).show();
+               }
+               if (seleccion.getCheckedRadioButtonId() == R.id.botonPatente) {
+                   //TODO: buscar patente
+                   Toast.makeText(Buscar.this, "El valor seleccionado es: "+ seleccion.getCheckedRadioButtonId(), Toast.LENGTH_SHORT).show();
+
+
+               }
+           }
+       });
+
+
+
+
+
+
+
     }
 }
