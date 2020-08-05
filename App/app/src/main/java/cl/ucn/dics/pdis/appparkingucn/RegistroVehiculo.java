75c2915c335a2cc6ff970b14b5ca6631221f634d
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-05 16:13:42 -0400

    feat(APP): add some functionalities to RegistroVehiculo

--- App/app/src/main/java/cl/ucn/dics/pdis/appparkingucn/RegistroVehiculo.java
+++ App/app/src/main/java/cl/ucn/dics/pdis/appparkingucn/RegistroVehiculo.java
@@ -2,13 +2,32 @@ package cl.ucn.dics.pdis.appparkingucn;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class RegistroVehiculo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_vehiculo);
+        Button botonRegistrar = findViewById(R.id.botonRegistrar);
+        RadioGroup entradaSalida = findViewById(R.id.tipoRegistro);
 
+        botonRegistrar.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                if (entradaSalida.getCheckedRadioButtonId() == R.id.botonRut) {
+                    //TODO: metodo ingreso
+                    Toast.makeText(RegistroVehiculo.this, "El valor seleccionado es: "+ entradaSalida.getCheckedRadioButtonId(), Toast.LENGTH_SHORT).show();
+                }
+                if (entradaSalida.getCheckedRadioButtonId() == R.id.botonPatente) {
+                    //TODO: buscar patente
+                    Toast.makeText(RegistroVehiculo.this, "El valor seleccionado es: "+ entradaSalida.getCheckedRadioButtonId(), Toast.LENGTH_SHORT).show();
+                }
+                //TODO: limpiar datos ingresados
+            }
+        });
     }
 }
