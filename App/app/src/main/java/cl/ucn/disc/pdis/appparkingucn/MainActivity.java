cca3aaaf70fa68162994f9082abe7392e9fc91bc
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-08-05 18:57:59 -0400

    Test(App): test the conection to C#

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/MainActivity.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/MainActivity.java
@@ -5,14 +5,26 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.*;
 import android.os.Bundle;
+import com.zeroc.Ice.*;
+import cl.ucn.disc.pdis.appparkingucn.dbinteraction.*;
+import cl.ucn.disc.pdis.simplescraper.zeroice.model.*;
 
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] args = {"one","two"};
+
+        try {
+
+            updater(args);
+        }catch (SQLException e){
+
+        }
 
         Button busquedaMain = findViewById(R.id.Buscar);
         Button registrarMain = findViewById(R.id.Registrar);
@@ -37,5 +49,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, RegistroVehiculo.class));
             }
         });
+    }
+
+    private void updater(String[] args) throws SQLException  {
+        // Initialize the Communicator.
+        try (Communicator communicator = Util.initialize(getInitializationData(args))) {
+
+            String name = Contratos.class.getSimpleName();
+
+            // Running in port 8080 same port that the Server.
+            ObjectPrx theProxy = communicator.stringToProxy(name + ":tcp -z -t 15000 -p 8080");
+
+            // Instance of Contratos from domain.ice
+            ContratosPrx contratos = ContratosPrx.checkedCast(theProxy);
+
+            // DB operations.
+            //DbInteraction dbInteraction = new DbInteraction();
+
+        }
+    }
+
+    private static InitializationData getInitializationData(String[] args) {
+
+        // Properties
+        final Properties properties = Util.createProperties(args);
+        properties.setProperty("Ice.Package.model", "cl.ucn.disc.pdis.simplescraper.zeroice");
+
+        // https://doc.zeroc.com/ice/latest/property-reference/ice-trace
+        // properties.setProperty("Ice.Trace.Admin.Properties", "1");
+        // properties.setProperty("Ice.Trace.Locator", "2");
+        // properties.setProperty("Ice.Trace.Network", "3");
+        // properties.setProperty("Ice.Trace.Protocol", "1");
+        // properties.setProperty("Ice.Trace.Slicing", "1");
+        // properties.setProperty("Ice.Trace.ThreadPool", "1");
+        // properties.setProperty("Ice.Compression.Level", "9");
+        //properties.setProperty("Ice.Plugin.Slf4jLogger.java", "cl.ucn.disc.pdis.simplescraper.zeroice.Slf4jLoggerPluginFactory");
+
+        InitializationData initializationData = new InitializationData();
+        initializationData.properties = properties;
+
+        return initializationData;
     }
 }
