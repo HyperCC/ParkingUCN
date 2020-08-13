ec42a231e085b226f2ff0bb2b2430f95466cc03a
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-13 14:59:54 -0400

    refactor(APP): change content from Buscar class and change layout context

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/Buscar.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/Buscar.java
@@ -1,24 +1,16 @@
 package cl.ucn.disc.pdis.appparkingucn;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.*;
 
 import cl.ucn.disc.pdis.appparkingucn.fragment.BuscarInicio;
-import cl.ucn.disc.pdis.appparkingucn.fragment.BuscarPersonaResultado;
-import cl.ucn.disc.pdis.appparkingucn.fragment.BuscarVehiculoResultado;
 
 
 public class Buscar extends AppCompatActivity {
 
-    FragmentTransaction transaction;
-
     Fragment fragmentInicioBuscar;
-    Fragment fragmentBuscarPersonaResultado;
-    Fragment fragmentBuscarVehiculoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +19,7 @@ public class Buscar extends AppCompatActivity {
         setContentView(R.layout.activity_buscar);
 
         fragmentInicioBuscar = new BuscarInicio();
-        fragmentBuscarPersonaResultado = new BuscarPersonaResultado();
-        fragmentBuscarVehiculoResultado = new BuscarVehiculoResultado();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments,fragmentInicioBuscar).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments, fragmentInicioBuscar).commit();
     }
 }
