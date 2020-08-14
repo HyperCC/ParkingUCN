33af1fd52703053c381d2960e13eb464a4699c89
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-08-14 02:33:07 -0400

    docs(App): add documentation

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/fragment/BuscarInicio.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/fragment/BuscarInicio.java
@@ -15,6 +15,9 @@ import cl.ucn.disc.pdis.appparkingucn.R;
  */
 public class BuscarInicio extends Fragment {
 
+    /**
+     *  Fragment constructor
+     */
     public BuscarInicio() {
         // Required empty public constructor
     }
@@ -22,6 +25,10 @@ public class BuscarInicio extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //TODO: Hacer elementos del fragmento funcionales
+
+	    //TODO: Hacer funcionales las transacciones de fragmentos
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_buscar_inicio, container, false);
     }
