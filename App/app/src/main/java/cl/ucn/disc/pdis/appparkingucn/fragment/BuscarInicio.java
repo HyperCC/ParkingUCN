f2c72964196aa97147cfd2583108a9537574a937
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-18 18:27:07 -0400

    feat(app): add button interactions functionality to BuscarInicio.java

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/fragment/BuscarInicio.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/fragment/BuscarInicio.java
@@ -2,12 +2,16 @@ package cl.ucn.disc.pdis.appparkingucn.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import cl.ucn.disc.pdis.appparkingucn.Buscar;
 import cl.ucn.disc.pdis.appparkingucn.R;
 
 /**
@@ -15,6 +19,9 @@ import cl.ucn.disc.pdis.appparkingucn.R;
  */
 public class BuscarInicio extends Fragment {
 
+    Button botonBuscar;
+    RadioGroup seleccion;
+    View vista;
     /**
      *  Fragment constructor
      */
@@ -22,14 +29,33 @@ public class BuscarInicio extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //TODO: Hacer elementos del fragmento funcionales
 
-	    //TODO: Hacer funcionales las transacciones de fragmentos
+        vista = inflater.inflate(R.layout.fragment_buscar_inicio, container, false);
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_buscar_inicio, container, false);
+        botonBuscar = (Button) vista.findViewById(R.id.botonBuscar);
+        seleccion = (RadioGroup) vista.findViewById(R.id.seleccion);
+
+
+        botonBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (seleccion.getCheckedRadioButtonId() == R.id.botonRut) {
+                    //TODO: metodo ingreso
+                    ((Buscar)getActivity()).resultadoPersona();
+                    Toast.makeText(getContext(), "El valor seleccionado es: "+ seleccion.getCheckedRadioButtonId(), Toast.LENGTH_SHORT).show();
+                }
+                if (seleccion.getCheckedRadioButtonId() == R.id.botonPatente) {
+                    //TODO: metodo salida
+                    ((Buscar)getActivity()).resultadoVehiculo();
+                    Toast.makeText(getContext(), "El valor seleccionado es: "+ seleccion.getCheckedRadioButtonId(), Toast.LENGTH_SHORT).show();
+                }
+                //TODO: limpiar datos ingresados
+            }
+        });
+        return vista;
     }
 }
