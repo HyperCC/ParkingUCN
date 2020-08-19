1a9e9a289b7a09c589d9ae95443bb3cbfc99a503
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-19 18:19:55 -0400

    feat(app): add buscar persona functionality to Buscar.java

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/Buscar.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/Buscar.java
@@ -4,8 +4,10 @@ import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import cl.ucn.disc.pdis.appparkingucn.fragment.*;
+import cl.ucn.disc.pdis.simplescraper.zeroice.model.Persona;
 
 
 public class Buscar extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class Buscar extends AppCompatActivity {
     Fragment fragmentInicioBuscar;
     Fragment fragmentPersonaResultado;
     Fragment fragmentVehiculoResultado;
+
+    Persona persona;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,100 @@ public class Buscar extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments, fragmentInicioBuscar).commit();
 
     }
+
+    public void reestructurarRut(String dato){
+
+        // Sample 5.808.054-3
+        String[] parts = dato.split("[-+*/=.<>]");
+        String datoTemp = "";
+
+        for (String split: parts) {
+
+            datoTemp = datoTemp+split;
+        }
+        //TODO: Reestructurar
+
+        int len = datoTemp.length();
+
+        if(len > 1){
+
+            char[] updatedArr = new char[len + 1];
+            datoTemp.getChars(0, len-1, updatedArr, 0);
+            updatedArr[len-1] = '-';
+            datoTemp.getChars(len-1, len, updatedArr, (len-1) + 1);
+
+            datoTemp = "";
+
+            for (char split: updatedArr) {
+
+                datoTemp = datoTemp+split;
+            }
+
+            len = datoTemp.length();
+        }
+
+        int verificadorPunto = 4;
+        int multiploPunto = 1;
+
+        if(len>1+(verificadorPunto*multiploPunto)){
+
+            int count = 0;
+            int i = len;
+            char[] updatedArr;
+
+            while(i>0){
+
+                if(len > 1+(verificadorPunto*multiploPunto)){
+
+                    updatedArr = new char[len + 1];
+
+                    if(count == 3 && i >= 2){
+
+                        datoTemp.getChars(0, i-2, updatedArr, 0);
+                        updatedArr[i-2] = '.';
+                        datoTemp.getChars(i-2, len, updatedArr, (i-2) + 1);
+
+                        datoTemp = "";
+
+                        for (char split: updatedArr) {
+
+                            datoTemp = datoTemp+split;
+                        }
+
+                        multiploPunto+=1;
+                        len = datoTemp.length();
+                        count = 0;
+                    }
+                }
+
+                count++;
+                i--;
+            }
+        }
+
+        consultarRut(datoTemp);
+    }
+
+    public void consultarRut(String datoTemp){
+
+
+        Communicator communicator = new Communicator();
+        persona = communicator.obtenerPersona(datoTemp);
+
+        if(persona != null){
+
+            resultadoPersona();
+        }else{
+
+            Toast.makeText(this, "PERSONA CON RUT: "+datoTemp+" NO ENCONTRADA", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Persona getRegistro(){
+
+        return this.persona;
+    }
+
 
     public void resultadoPersona() {
 
