8544eb9e895597db4c71b77a3d0f34ce85575e4f
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-20 00:20:53 -0400

    feat(app): add patente format functionality to RegistroVehiculo.java

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/RegistroVehiculo.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/RegistroVehiculo.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import cl.ucn.disc.pdis.appparkingucn.fragment.*;
 
@@ -19,5 +20,72 @@ public class RegistroVehiculo extends AppCompatActivity {
         fragmentInicioRegistro = new RegistrarInicio();
 
         getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments, fragmentInicioRegistro).commit();
+    }
+
+    public void limpiarPatente(String dato) {
+
+        // Sample AA-SS-DD
+        String[] parts = dato.split("[-+*/=.<>@#$%^&)(]");
+        String datoTemp = "";
+
+        for (String split : parts) {
+
+            datoTemp = datoTemp + split;
+        }
+
+        if (datoTemp.length() == 6) {
+
+            reestructurarPatente(datoTemp);
+        } else {
+            Toast.makeText(this, "La patente debe ser unicamente de 6 digitos.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void reestructurarPatente(String datoTemp) {
+
+
+        int len = datoTemp.length();
+
+        int verificadorPunto = 2;
+        int multiploPunto = 1;
+
+        if(len>(verificadorPunto*multiploPunto)){
+
+
+            int count = 0;
+            int i = len;
+            char[] updatedArr;
+
+            while(i>0){
+
+                if(len > (verificadorPunto*multiploPunto)){
+
+                    updatedArr = new char[len + 1];
+
+                    if(count == 2 && i > 0){
+
+                        datoTemp.getChars(0, i, updatedArr, 0);
+                        updatedArr[i] = '-';
+                        datoTemp.getChars(i, len, updatedArr, i + 1);
+
+                        datoTemp = "";
+
+                        for (char split: updatedArr) {
+
+                            datoTemp = datoTemp+split;
+                        }
+
+                        multiploPunto+=1;
+                        len = datoTemp.length();
+                        count = 0;
+                    }
+                }
+
+                count++;
+                i--;
+            }
+        }
+
+        Toast.makeText(this, datoTemp, Toast.LENGTH_SHORT).show();
     }
 }
