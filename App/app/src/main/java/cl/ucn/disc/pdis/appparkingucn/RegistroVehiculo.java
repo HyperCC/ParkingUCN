654d817541167d9c3d4f7e4ce20e4b9541e69ae9
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-20 18:52:56 -0400

    feat(App): add some functionalities to RegistroVehiculo.java

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/RegistroVehiculo.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/RegistroVehiculo.java
@@ -6,25 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import cl.ucn.disc.pdis.appparkingucn.fragment.*;
-import cl.ucn.disc.pdis.simplescraper.zeroice.model.Persona;
-import cl.ucn.disc.pdis.simplescraper.zeroice.model.Vehiculo;
+import cl.ucn.disc.pdis.simplescraper.zeroice.model.*;
+
+
 
 public class RegistroVehiculo extends AppCompatActivity {
 
-    Fragment fragmentInicioRegistro;
+    Fragment fragmentRegistrarInicio;
+    Fragment fragmentRegistrarConfirmacion;
 
-    Persona persona;
-    Vehiculo vehiculo;
+    private Persona persona;
+    private Vehiculo vehiculo;
+    private String tipoRegistro;
+
+    private String fechaRegistro;
+    private String horaRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_vehiculo);
 
-        fragmentInicioRegistro = new RegistrarInicio();
+        fragmentRegistrarInicio = new RegistrarInicio();
 
-        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments, fragmentInicioRegistro).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments, fragmentRegistrarInicio).commit();
     }
 
     public void limpiarPatente(String dato) {
@@ -38,16 +47,18 @@ public class RegistroVehiculo extends AppCompatActivity {
             datoTemp = datoTemp + split;
         }
 
+        datoTemp = datoTemp.toUpperCase();
+
         if (datoTemp.length() == 6) {
 
             reestructurarPatente(datoTemp);
         } else {
+
             Toast.makeText(this, "La patente debe ser unicamente de 6 digitos.", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void reestructurarPatente(String datoTemp) {
-
 
         int len = datoTemp.length();
 
@@ -55,7 +66,6 @@ public class RegistroVehiculo extends AppCompatActivity {
         int multiploPunto = 1;
 
         if(len>(verificadorPunto*multiploPunto)){
-
 
             int count = 0;
             int i = len;
@@ -117,14 +127,99 @@ public class RegistroVehiculo extends AppCompatActivity {
 
         if(persona != null){
 
-            // TODO: Fragment transaction and deploy DATA
-            /*
+            fragmentRegistrarConfirmacion = new RegistrarConfirmacion();
+            generarFecha();
+
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.contenedorFragments, fragmentInicioRegistro).addToBackStack(null).commit();*/
+                    .replace(R.id.contenedorFragments, fragmentRegistrarConfirmacion).addToBackStack(null).commit();
         }else{
 
             Toast.makeText(this, "PERSONA CON RUT: "+rut+" NO ENCONTRADA", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void generarFecha(){
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        this.fechaRegistro = simpleDateFormat.format(calendar.getTime());
+
+        simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
+
+        this.horaRegistro = simpleDateFormat.format(calendar.getTime());
+    }
+
+    // TODO: REQUIERE CONEXION
+    public void generarRegistro(){
+
+        Communicator communicator = new Communicator();
+
+        Registro registro = new Registro();
+
+        registro.patente = vehiculo.patente;
+        registro.responsable = persona.rut;
+        registro.fecha = fechaRegistro;
+        registro.hora = horaRegistro;
+        registro.estado = transformarTipoRegistro();
+
+        Registro registroVerificar = communicator.crearRegistro(registro);
+
+        if(registroVerificar != null){
+
+            Toast.makeText(this, "EL REGISTRO FUE INGRESADO CON EXITO", Toast.LENGTH_SHORT).show();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedorFragments, fragmentRegistrarInicio).addToBackStack(null).commit();
+
+        }else{
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.contenedorFragments, fragmentRegistrarInicio).addToBackStack(null).commit();
+            Toast.makeText(this, "EL REGISTRO NO PUDO SER INGRESADO", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Estado transformarTipoRegistro(){
+
+        if(this.tipoRegistro == "Entrada"){
+
+            return Estado.ENTRADA;
+        }else{
+
+            return Estado.SALIDA;
+        }
+    }
+
+    public void setTipoRegistro(String tipoRegistro){
+
+        this.tipoRegistro = tipoRegistro;
+    }
+
+    public String getTipoRegistro(){
+
+        return this.tipoRegistro;
+    }
+
+    public Persona getPersona(){
+
+        return this.persona;
+    }
+
+    public Vehiculo getVehiculo(){
+
+        return this.vehiculo;
+    }
+
+    public String getFechaRegistro(){
+
+        return this.fechaRegistro;
+    }
+
+    public String getHoraRegistro(){
+
+        return this.horaRegistro;
     }
 }
