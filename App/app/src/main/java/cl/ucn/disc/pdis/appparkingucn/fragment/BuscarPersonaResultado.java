1439c70bff29eca133becdc85c859b28c60f1c8a
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-08-21 02:03:00 -0400

    docs(App): add documentation to BuscarPersonaResultado.java

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/fragment/BuscarPersonaResultado.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/fragment/BuscarPersonaResultado.java
@@ -36,27 +36,31 @@ import cl.ucn.disc.pdis.simplescraper.zeroice.model.Registro;
  */
 public class BuscarPersonaResultado extends Fragment {
 
+    // Variable declaration.
     Registro registro;
     Persona persona;
 
-    // Diseño vista
+    // Header view variables.
     TextView tituloBuscarResultado;
     TextView tipoDatoBuscado;
+
+    // Variables for patente format.
     TextView datoBuscado;
     TextView datoRegistroBuscado;
 
-
-
+    // Variable for Persona display name.
     TextView nombrePersonaBuscada;
 
+    // Variable for date and time format design.
     TextView tipoFechaRegistro;
     TextView tipoHoraRegistro;
 
+    // Variable for date and time.
     TextView fechaRegistro;
     TextView horaRegistro;
 
+    // Variable for view transition.
     View vista;
-
 
     /**
      *  Fragment constructor
@@ -71,58 +75,69 @@ public class BuscarPersonaResultado extends Fragment {
 
         vista = inflater.inflate(R.layout.fragment_buscar_persona_resultado, container, false);
 
-        // Encabezado
+        // Header data display.
         tituloBuscarResultado = (TextView) vista.findViewById(R.id.tituloBuscarResultado);
         tipoDatoBuscado = (TextView) vista.findViewById(R.id.tipoDatoBuscado);
 
-
+        // Display for the Persona name.
         nombrePersonaBuscada = (TextView) vista.findViewById(R.id.nombrePersonaBuscada);
 
+        // display for rut or patente.
         datoBuscado = (TextView) vista.findViewById(R.id.datoBuscado);
         datoRegistroBuscado = (TextView) vista.findViewById(R.id.datoRegistroBuscado);
 
+        // Date format display.
         tipoFechaRegistro = (TextView) vista.findViewById(R.id.tipoFechaRegistro);
         tipoHoraRegistro = (TextView) vista.findViewById(R.id.tipoHoraRegistro);
 
+        // Date display.
         fechaRegistro = (TextView) vista.findViewById(R.id.fechaRegistro);
         horaRegistro = (TextView) vista.findViewById(R.id.horaRegistro);
 
-        //--
+        // Gets a Registro from activity Buscar.
         registro = ((Buscar)getActivity()).getRegistro();
+
+        // Gets a Persona from activity Buscar.
         persona = ((Buscar)getActivity()).getPersona();
 
-        //--
-
+        // Sets the name of Persona.
         nombrePersonaBuscada.setText(persona.nombre);
 
+        // if estado equals entrada.
         if(registro.estado == Estado.ENTRADA){
 
+            // Sets the date and time display format to entrada.
             tipoFechaRegistro.setText("Fecha Entrada");
             tipoHoraRegistro.setText("Hora Entrada");
         }else{
 
+            // Sets the date and time display format to salida.
             tipoFechaRegistro.setText("Fecha Salida");
             tipoHoraRegistro.setText("Hora Salida");
         }
 
+        // Sets the date and time.
         fechaRegistro.setText(registro.fecha);
         horaRegistro.setText(registro.hora);
 
+        // if tipoDato equals responsable.
         if(((Buscar)getActivity()).getTipoDato() == "responsable"){
 
+            // Sets the header display to RUT format and displays the patente in the table.
             tituloBuscarResultado.setText("RESULTADO PARA RUT");
             tipoDatoBuscado.setText(registro.responsable);
             datoBuscado.setText("Patente: ");
             datoRegistroBuscado.setText(registro.patente);
         }else{
 
+            // Sets the header display to Patente format and displays the RUT in the table.
             tituloBuscarResultado.setText("RESULTADO PARA PATENTE");
             tipoDatoBuscado.setText(registro.patente);
             datoBuscado.setText("RUT: ");
             datoRegistroBuscado.setText(registro.responsable);
         }
 
-        // Inflate the layout for this fragment
+        // View return.
         return vista;
     }
 }
