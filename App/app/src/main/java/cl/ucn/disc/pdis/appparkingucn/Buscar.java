8e47376ddd4448fef835d9cc3ec29feaf67e872f
Jorge Ignacio Pizarro Tapia <jpt010@alumnos.ucn.cl>
2020-08-21 01:10:11 -0400

    feat(App): add patente structurer functionality to Buscar.java

--- App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/Buscar.java
+++ App/app/src/main/java/cl/ucn/disc/pdis/appparkingucn/Buscar.java
@@ -123,6 +123,74 @@ public class Buscar extends AppCompatActivity {
         consultarRegistro(datoTemp);
     }
 
+    public void limpiarPatente(String dato) {
+
+        // Sample AA-SS-DD
+        String[] parts = dato.split("[-+*/=.<>@#$%^&)(]");
+        String datoTemp = "";
+
+        for (String split : parts) {
+
+            datoTemp = datoTemp + split;
+        }
+
+        datoTemp = datoTemp.toUpperCase();
+
+        if (datoTemp.length() == 6) {
+
+            reestructurarPatente(datoTemp);
+        } else {
+
+            Toast.makeText(this, "La patente debe ser unicamente de 6 digitos.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void reestructurarPatente(String datoTemp) {
+
+        int len = datoTemp.length();
+
+        int verificadorPunto = 2;
+        int multiploPunto = 1;
+
+        if(len>(verificadorPunto*multiploPunto)){
+
+            int count = 0;
+            int i = len;
+            char[] updatedArr;
+
+            while(i>0){
+
+                if(len > (verificadorPunto*multiploPunto)){
+
+                    updatedArr = new char[len + 1];
+
+                    if(count == 2 && i > 0){
+
+                        datoTemp.getChars(0, i, updatedArr, 0);
+                        updatedArr[i] = '-';
+                        datoTemp.getChars(i, len, updatedArr, i + 1);
+
+                        datoTemp = "";
+
+                        for (char split: updatedArr) {
+
+                            datoTemp = datoTemp+split;
+                        }
+
+                        multiploPunto+=1;
+                        len = datoTemp.length();
+                        count = 0;
+                    }
+                }
+
+                count++;
+                i--;
+            }
+        }
+
+        consultarRegistro(datoTemp);
+    }
+
     public void consultarRegistro(String datoTemp){
 
         Communicator communicator = new Communicator();
